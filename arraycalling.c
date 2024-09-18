1c029e1ce1e5bf3dd7f3f291b70a693f1c7a7dfc
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-09-18 21:40:52 +0530

    called by array index value

---
+++ arraycalling.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a[]={78,90,34,56,89};
+    int i;
+    //we can also use for loop to print the array elements
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",a[i]);
+       //print the value of a[i] and then go to the next line
+    }
+    }
---
+++ arrayelementcallbyindexvalue.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a[]={78,90,34,56,89,};
+    int i;
+    //we can also use for loop to print the array elements
+    {
+        printf("%d\n",a[3]); //by defualt the array index start from 0
+        //print the 4th element of the array a which is 56
+            }
+}
